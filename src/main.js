b9e1506a641e3941651054d33aef9a8f6af216f1
Dripcoding <dripcoding@gmail.com>
2020-10-31 19:29:21 -0700

    import images

--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
 // images
+import './assets/images/bg-pattern-bottom-desktop.svg'
+import './assets/images/bg-pattern-bottom-mobile.svg'
+import './assets/images/bg-pattern-top-desktop.svg'
+import './assets/images/bg-pattern-top-mobile.svg'
+import './assets/images/favicon-32x32.png'
 import './assets/images/image-anne.jpg'
+import './assets/images/image-colton.jpg'
+import './assets/images/icon-star.svg'
+import './assets/images/image-irene.jpg'
+
 // styles
-import './styles/reset.scss'
 import './styles/main.scss'
+import './styles/reset.scss'
 
